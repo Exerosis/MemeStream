f4b49d4153689bf1469847259e5e2718b7c29d6e
Exerosis <theexerosisgaming@gmail.com>
2017-09-03 19:23:40 -0400

    ~Updates

--- app/src/main/java/stream/meme/app/stream/StreamController.java
+++ app/src/main/java/stream/meme/app/stream/StreamController.java
@@ -1,7 +1,6 @@
 package stream.meme.app.stream;
 
 import android.support.v7.util.SortedList;
-import android.widget.Toast;
 
 import com.google.common.collect.Lists;
 import com.pacoworks.rxsealedunions2.Union2;
@@ -20,6 +19,8 @@ import stream.meme.app.MemeStream;
 import stream.meme.app.R;
 import stream.meme.app.bisp.BISPDatabindingController;
 import stream.meme.app.databinding.StreamViewBinding;
+
+import static stream.meme.app.stream.State.Load.*;
 
 public class StreamController extends BISPDatabindingController<Intents, StreamViewBinding, State> {
     private final MemeStream memeStream;
@@ -41,7 +42,7 @@ public class StreamController extends BISPDatabindingController<Intents, StreamV
     public BiConsumer<Intents, Binder<StreamViewBinding>> getViewToIntentBinder() {
         return (intents, view) -> {
             SortedList list = null;
-            list.end
+            list
         };
     }
 
@@ -62,10 +63,11 @@ public class StreamController extends BISPDatabindingController<Intents, StreamV
         return new Intents();
     }
 
-    private State reduceState(State state, Union3<State.LoadNext, State.Refresh, State.LoadFirst> partialState) throws Exception {
-       state.error = partialState.join(loadNext -> loadNext.error, refresh -> refresh.error, loadFirst -> loadFirst.error);
+    private State reduceState(State state, Union3<State.Load.First, State.Load.Next, State.Load.Refresh> loadState) throws Exception {
+        state.error = loadState.join(First::error, Next::error, Refresh::error);
+        state.memes = loadState.join()
 
-        partialState.continued(loadNext -> {
+                partialState.continued(loadNext -> {
             state.loadNextError = loadNext.error;
             state.loadNextLoading = loadNext.loading;
             for (Meme meme : loadNext.memes)
@@ -102,101 +104,54 @@ class State {
     Throwable error = new Throwable();
     Deque<Meme> memes = new ArrayDeque<>();
 
-    static class LoadFirst {
+    static class Load {
         final boolean loading;
         final List<Meme> memes;
         final Throwable error;
 
-        private LoadFirst(boolean loading, List<Meme> memes, Throwable error) {
-            this.loading = loading;
+        private Load(List<Meme> memes) {
+            this.loading = false;
             this.memes = memes;
+            this.error = null;
+        }
+
+        private Load() {
+            this.loading = true;
+            this.memes = new ArrayList<>();
+            this.error = null;
+        }
+
+        private Load(Throwable error) {
+            this.loading = false;
+            this.memes = new ArrayList<>();
             this.error = error;
         }
 
-        static class Error extends LoadFirst {
-
-            Error(Throwable error) {
-                super(false, new ArrayList<>(), error);
-            }
+        public List<Meme> memes() {
+            return memes;
         }
 
-        static class Loaded extends LoadFirst {
-
-            Loaded(List<Meme> memes) {
-                super(false, memes, null);
-            }
+        public Throwable error() {
+            return error;
         }
 
-        static class Loading extends LoadFirst {
-            Loading() {
-                super(false, new ArrayList<>(), null);
-            }
-        }
-    }
-
-    static class LoadNext {
-        final boolean loading;
-        final List<Meme> memes;
-        final Throwable error;
-
-        private LoadNext(boolean loading, List<Meme> memes, Throwable error) {
-            this.loading = loading;
-            this.memes = memes;
-            this.error = error;
+        public boolean loading() {
+            return loading;
         }
 
-        static class Error extends LoadNext {
+        static class Refresh extends Load {
 
-            Error(Throwable error) {
-                super(false, new ArrayList<>(), error);
-            }
         }
 
-        static class Loaded extends LoadNext {
+        static class First extends Load {
 
-            Loaded(List<Meme> memes) {
-                super(false, memes, null);
-            }
         }
 
-        static class Loading extends LoadNext {
-            Loading() {
-                super(false, new ArrayList<>(), null);
-            }
-        }
-    }
+        static class Next extends Load {
 
-    static class Refresh {
-        final boolean loading;
-        final List<Meme> memes;
-        final Throwable error;
-
-        private Refresh(boolean loading, List<Meme> memes, Throwable error) {
-            this.loading = loading;
-            this.memes = memes;
-            this.error = error;
-        }
-
-        static class Error extends Refresh {
-
-            Error(Throwable error) {
-                super(false, new ArrayList<>(), error);
-            }
-        }
-
-        static class Loaded extends Refresh {
-
-            Loaded(List<Meme> memes) {
-                super(false, memes, null);
-            }
-        }
-
-        static class Loading extends Refresh {
-            Loading() {
-                super(false, new ArrayList<>(), null);
-            }
         }
     }
 
 }
+
 
